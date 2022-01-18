1e956349d7defa59182120a962a993e38572811a
Flavio Vieira <flaviorvieirafilho@gmail.com>
2022-01-18 19:49:08 -0300

    Atualização de melhoria

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ var id = 1;
 
 const criarDiv = () => {
 
-    
+    for (let i = 1; i < 10; i++) {
         dados = fetch(`https://pokeapi.co/api/v2/pokemon/${id++}`)
             .then(response => response.json())
             .then(result => {
@@ -12,45 +12,39 @@ const criarDiv = () => {
                 var tipo = result.types[0].type.name;
 
                 recebeDados(idPoke, pokemon, tipo)
+                
             });
+        }
     
 }
 
-for (let i = 1; i < 8; i++) {
-    criarDiv()
-}
+criarDiv()
 
 function recebeDados(id, pokemon, tipo) {
 
     const listPokemons = document.querySelector('[data-div-app]');
     const divPokemon = document.createElement('div');
-    divPokemon.classList.add('content');
+    divPokemon.classList.add('col-4', 'my-3');
 
-    const conteudo = `
-    <div class="grid card-novo" >
-    <div class="card" style=" width: 26rem;">
-        <div class="card-body">
-        <h4 class="card-title">#${id}</h4>
-        <img height="180px" width="180px" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg" class="img-cards" alt="...">
-            
-            <h5 class="card-title">#${pokemon}</h5>
-            <p class="card-text">${tipo}</p>
-            <a href="#" class="btn btn-dark">Usar Pokemon</a>
+    const conteudo = `    
+    <div class="card ">
+            <div class="card-body ${tipo}">
+                <h4 class="card-title">#${id}</h4>
+                <img height="180px" width="180px" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg" class="img-cards" alt="...">
+                <h5 class="card-title">#${pokemon}</h5>
+                <p class="card-text">${tipo}</p>
+                
+            </div>
         </div>
-    </div>
-    </div>
     `
-
-   
     divPokemon.innerHTML = conteudo;
     listPokemons.appendChild(divPokemon);
 }
 
+
 const botaoCarregar = document.querySelector('[data-button-louding]');
 botaoCarregar.addEventListener('click', () => {
-    for (let index = 1; index < 8; index++) {
-        criarDiv()
-        }
+    criarDiv()
 });
 
 
